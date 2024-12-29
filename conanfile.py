5dd1b53600c5ad4b2a50b59ad45f681d5f02326d
Philippe Steinmann <philippe.steinmann@gmail.com>
2024-12-29 10:35:44 +0100

    Update conanfile to Conan 2
    GL-3

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,10 @@
-from conans import ConanFile, tools
-import os
+from conan import ConanFile
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 
 # NOTE: this recipe is only to install dependnecies.
 class MdtLibraryUmlConan(ConanFile):
-  name = "mdt-library-uml"
+  name = "mdtlibraryuml"
   #version = "0.1"
   license = "BSD 3-Clause"
   url = "https://gitlab.com/scandyna/mdt-library-uml"
@@ -11,14 +12,5 @@ class MdtLibraryUmlConan(ConanFile):
   settings = "os", "compiler", "build_type", "arch"
   generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv"
 
-  # When using --profile:build xx and --profile:host xx ,
-  # the dependencies declared in build_requires and tool_requires
-  # will not generate the required files.
-  # see:
-  # - https://github.com/conan-io/conan/issues/10272
-  # - https://github.com/conan-io/conan/issues/9951
   def build_requirements(self):
-    # Due to a issue using GitLab Conan repository,
-    # version ranges are not possible.
-    # See https://gitlab.com/gitlab-org/gitlab/-/issues/333638
-    self.tool_requires("MdtCMakeModules/0.19.3@scandyna/testing", force_host_context=True)
+    self.test_requires("mdtcmakemodules/0.20.0@scandyna/testing")
